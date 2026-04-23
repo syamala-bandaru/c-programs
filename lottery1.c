03923c8c8fca2df814ce3da987ec3287db64ca0a
syamala-bandaru <syamalabandaru9@gmail.com>
2026-04-23 12:26:40 +0530

    Add files via upload

---
+++ lottery1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main() {
+    int a,b,c;
+    printf("Enter the value in a:\n");
+    scanf("%d",&a);
+    printf("Enter the value in b:\n");
+    scanf("%d",&b);
+    printf("Enter the value in c:\n");
+    scanf("%d",&c);
+    if(a==2&&b==2&&c==2)
+    {
+        printf("10");
+    }
+    else if(a==b&&b==c&&c==a)
+    {
+        printf("5");
+    }
+    else if(b!=a&&c!=a)
+    {
+        printf("1");
+    }
+    else
+    {
+        printf("0");
+    }
+}
