445390b72d9dbdf24b2588a6d0ba8b5cec009dc0
syamala-bandaru <syamalabandaru9@gmail.com>
2026-04-23 12:27:46 +0530

    Add files via upload

---
+++ evennum.c
@@ -0,0 +1,9 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int a;
+    printf("Enter the A value \n");
+    scanf("%d",&a);
+    printf("A is even value i.e is :%d",a%2==0);
+}
