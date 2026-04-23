8735cafa136e17b1f6b7d38448609f4d5f169025
syamala-bandaru <syamalabandaru9@gmail.com>
2026-04-23 12:25:16 +0530

    Add files via upload

---
+++ notpositive.c
@@ -0,0 +1,9 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   int a;
+   printf("Enter the A value \n");
+   scanf("%d",&a);
+   printf("%d A is a not +ve value i.e is:%d",a,a<0);
+}
